2d39496ea0b2c0bd76b325efd3d312ba2cf32b0f
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-06-05 22:57:42 +0600

    readme md file added

--- README.md
+++ README.md
@@ -1,8 +1,36 @@
-# React + Vite
+# Foodzy Client
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## Project Overview
 
-Currently, two official plugins are available:
+**Foodzy Client** is a web application designed to offer an enhanced food-related user experience. This application uses modern web technologies such as React, Firebase, and Vite, along with powerful tools like TailwindCSS for styling and ESLint for code quality assurance.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## live website link
+
+You can access the live version of the project at: [Foodzy Client Live Demo](https://foodzy-client.vercel.app/)
+
+## Features
+
+- **User Authentication**: Integrated with Firebase for secure and easy user authentication.
+- **Data Storage**: Uses localforage for offline data storage capabilities.
+- **Search and Sort**: Enhanced search and sorting functionality using match-sorter and sort-by.
+- **Responsive UI**: Designed with TailwindCSS and daisyUI for responsive and visually appealing interfaces.
+- **Form Handling**: Efficient form management with react-hook-form.
+- **Routing**: Client-side routing using react-router-dom.
+- **Alerts and Notifications**: Integrated SweetAlert2 for beautiful alerts and notifications.
+
+## Prerequisites
+
+
+Ensure you have the following installed on your development machine:
+
+- Node.js (version 14 or higher)
+- npm (version 6 or higher)
+
+## Setup Instructions
+
+### 1. Clone the Repository
+
+```bash
+git clone https://github.com/faridul22/foodzy-client.git
+git clone https://github.com/faridul22/foodzy-server.git
+cd foodzy-client
--- src/pages/AllFoods/AllFoods.jsx
+++ src/pages/AllFoods/AllFoods.jsx
@@ -3,7 +3,21 @@ import MenuCard from "../../components/HomeComponents/MenuCard";
 
 const AllFoods = () => {
     const [items, setItems] = useState([]);
+    console.log(items)
 
+    // -----------search start----------
+    const [searchItem, setSearchItem] = useState(items);
+    const [searchText, setSearchText] = useState("");
+
+    const handleSearch = () => {
+        fetch(`https://foodzy-server-wd6a.vercel.app/getToyNameByText/${searchText}`)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setSearchItem(data);
+            });
+    };
+    // -----------search end------------
 
     useEffect(() => {
         fetch("https://foodzy-server-wd6a.vercel.app/items")
@@ -19,9 +33,19 @@ const AllFoods = () => {
                 <h1 className="text-2xl font-bold">All Our Recipes</h1>
                 <p className="">Explore our diverse collection of recipes for every occasion</p>
             </div>
+            {/* -----------------------Search start------------------- */}
+            <div className="navbar container mx-auto bg-orange-50 rounded-md mb-3 mt-7">
+                <div className="w-3/4 mx-auto">
+                    <div className="form-control w-[700px] ml-10">
+                        <input onChange={(e) => setSearchText(e.target.value)} type="text" placeholder="Search by name" className="input input-bordered" />{""}
+                    </div>
+                    <button className="btn bg-orange-400 hover:bg-orange-600 hover:text-white border-0 ml-10 text-white" disabled={searchText == ""} onClick={handleSearch}>Search</button>
+                </div>
+            </div>
+            {/* --------------End---------------- */}
             <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 w-3/4 mx-auto'>
                 {
-                    items?.map(item => <MenuCard key={item._id} itemData={item} />)
+                    searchItem == [] ? searchItem?.map(item => <MenuCard key={item._id} itemData={item} />) : items?.map(item => <MenuCard key={item._id} itemData={item} />)
                 }
             </div>
         </div>
